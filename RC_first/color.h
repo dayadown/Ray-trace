2ab4322fe0f7afcdc4e96cb5d4af2253d1467538
dayadown <1593441796@qq.com>
2024-01-30 11:37:15 +0800

    Using Gamma Correction for Accurate Color Intensity
    使用伽玛校正准确的色彩强度

--- RC_first/color.h
+++ RC_first/color.h
@@ -4,6 +4,12 @@
 #include <iostream>
 
 using color = vec3;
+// 伽马矫正函数 gamma=2，即做1/2为指数的指数运算
+inline double linear_to_gamma(double linear_component)
+{
+    return sqrt(linear_component);
+}
+
 
 void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
     //传入的pixel_color为[0,1]的颜色值
@@ -13,11 +19,17 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
     auto b = pixel_color.z();
 
     // Divide the color by the number of samples.
-    // 将颜色做平均处理（计算真实贡献）
+    // 将颜色做平均处理（计算真实贡献），传入的color是n条光线的总和
     auto scale = 1.0 / samples_per_pixel;
     r *= scale;
     g *= scale;
     b *= scale;
+
+    // Apply the linear to gamma transform.
+    // 伽马矫正
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Write the translated [0,255] value of each color component.
     // 转换并输出[0,255]区间的颜色值
